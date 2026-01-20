cd4f5b7b5492ec39f64bc31f0862c66e89dd8f6a
Alex <klawdee@mail.ru>
2026-01-20 10:50:09 +0300

    add: routes

--- internal/routes/main.go
+++ internal/routes/main.go
@@ -9,4 +9,16 @@ func V1(r *gin.RouterGroup) {
 	// Define a simple GET endpoint
 	r.GET("/ping", handler.Ping)
 
+	r.GET("/user/all", handler.GetAllUsers)
+	r.POST("/user/regisger", handler.RegisterUser)
+	r.POST("/user/auth", handler.AuthUser)
+
+	r.GET("/event/owner/:ownerId", handler.GetEventsByOwner)
+	r.GET("/event/find/:name", handler.GetEventsByName)
+
+	r.POST("/event", handler.CreateEvent)
+
+	r.GET("/event/:eventId/attendees", handler.GetAttendees)
+	r.POST("/event/addAttendee", handler.AddAttendee)
+
 }
