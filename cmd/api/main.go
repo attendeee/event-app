7ed22a1f3726eda56256d5469cc0ee8c7d046c19
Alex <klawdee@mail.ru>
2026-01-20 10:55:40 +0300

    add: cors

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/attendeee/event-app/internal/routes"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,6 +13,15 @@ func main() {
 	// Create a Gin router with default middleware (logger and recovery)
 	r := gin.Default()
 
+	corsConfig := cors.Config{
+		AllowAllOrigins: true,
+		AllowOrigins:    []string{"http://example.com"},
+		AllowMethods:    []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:    []string{"Origin", "Content-Type", "Authorization"},
+		MaxAge:          12 * time.Hour,
+	}
+
+	r.Use(cors.New(corsConfig))
 	v1 := r.Group("/api/v1")
 
 	routes.V1(v1)
