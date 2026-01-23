564cca941c6c305de6c37fcd21c45148263518fd
Alex <klawdee@mail.ru>
2026-01-23 16:32:41 +0300

    add: register swagger

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,11 +4,22 @@ import (
 	"log"
 	"time"
 
+	_ "github.com/attendeee/event-app/cmd/docs"
+
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+
 	"github.com/attendeee/event-app/internal/routes"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
+// @title           Swagger Example API
+// @version         1.0
+// @description     This is a sample server celler server.
+// @host      localhost:8080
+// @BasePath  /api/v1
+// @securityDefinitions.basic  BasicAuth
 func main() {
 	// Create a Gin router with default middleware (logger and recovery)
 	r := gin.Default()
@@ -24,6 +35,8 @@ func main() {
 	v1 := r.Group("/api/v1")
 
 	routes.V1(v1)
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	// Start server on port 8080 (default)
 	// Server will listen on 0.0.0.0:8080 (localhost:8080 on Windows)
